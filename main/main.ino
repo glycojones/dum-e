a21bb633e5fb1fb44ad2ca57c0b640de9314d12c
Jon Agirre <jon.agirre@york.ac.uk>
2019-12-02 19:59:22 +0000

    Support for new functions

--- main/main.ino
+++ main/main.ino
@@ -8,10 +8,18 @@ const int right_positive = 10;
 const int right_negative = 9;
 
 // Sonar control pins
-const int sonar_send = 2;
-const int sonar_receive = 3;
+#define sonar_send A1;
+#define sonar_receive A0;
 
 const int buzzer = 13;
+
+void play_warning();
+void play_stop();
+void move_front();
+void move_back();
+void turn_right();
+void turn_left();
+void stop_car();
 
 void setup() {
   // to be run just once
@@ -19,21 +27,80 @@ void setup() {
   pinMode (left_negative, OUTPUT);
   pinMode (right_positive, OUTPUT);
   pinMode (right_negative, OUTPUT);
-
 }
 
 void loop() {
+  move_front();
+  delay(5000);
+  play_warning();
+  turn_left();
+  delay(1000);
+  move_front();
+  delay(5000);
+  play_warning();
+  move_back();
+  delay(5000);
+  play_stop();
+  stop_car();
+}
+
+void move_front () {
   digitalWrite (left_positive, HIGH);
   digitalWrite (left_negative, LOW);
   digitalWrite (right_positive, HIGH);
   digitalWrite (right_negative, LOW);
-  noTone(buzzer);
+}
+
+void move_back() {
+  digitalWrite (left_positive, LOW);
+  digitalWrite (left_negative, HIGH);
+  digitalWrite (right_positive, LOW);
+  digitalWrite (right_negative, HIGH);
+}
+
+void turn_right () {
+  digitalWrite (left_positive, HIGH);
+  digitalWrite (left_negative, LOW);
+  digitalWrite (right_positive, LOW);
+  digitalWrite (right_negative, HIGH);
+}
+
+void turn_left () {
+  digitalWrite (left_positive, LOW);
+  digitalWrite (left_negative, HIGH);
+  digitalWrite (right_positive, HIGH);
+  digitalWrite (right_negative, LOW);
+}
+
+void stop_car () {
+  digitalWrite (left_positive, LOW);
+  digitalWrite (left_negative, LOW);
+  digitalWrite (right_positive, LOW);
+  digitalWrite (right_negative, LOW);  
+}
+
+void play_warning () {
   tone (buzzer, 1000);
-  delay (1000);
-  tone (buzzer, 2000);
-  delay (1000);
-  tone (buzzer, 2500);
-  delay(1000);
+  delay (500);
   noTone (buzzer);
-  delay (5000);
+  delay (500);
+  tone (buzzer, 1000);
+  delay (1500);
+  noTone (buzzer);
+}
+
+void play_stop() {
+  tone (buzzer, 4000);
+  delay (400);
+  noTone (buzzer);
+  delay (400);
+  tone (buzzer, 4000);
+  delay (400);
+  tone (buzzer, 4000);
+  delay (400);
+  noTone (buzzer);
+  delay (400);
+  tone (buzzer, 4000);
+  delay (400);
+  noTone (buzzer);
 }
